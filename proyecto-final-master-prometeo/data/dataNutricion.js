397fe18c7ecfba7b26321414b8d7b6800e1151ed
deiybg <deibygorrin@gmail.com>
2024-08-28 17:12:49 +0200

    configurando la data nutricional

--- proyecto-final-master-prometeo/data/dataNutricion.js
+++ proyecto-final-master-prometeo/data/dataNutricion.js
@@ -2,10 +2,90 @@ export const dataNutritional = async (quantity,unit,food) =>{
 try {
     const data = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=${import.meta.env.VITE_APPLICATION_ID}&app_key=${import.meta.env.VITE_APPLICATION_KEY}&ingr=${quantity}%20${unit}%20${food}`);
     const dataJson = await data.json();
-    const dataFood = dataJson.totalNutrients;
-    console.log(dataJson);
+    const totalNutrientsKcal = dataJson.totalNutrientsKCal;
+    const totalNutrients = dataJson.totalNutrients;
+    const totalNutrientsDaily = dataJson.totalDaily;
     
+    console.log(totalNutrients);
     
+
+    
+    const getInfoTotalMacro = (totalNutrientsKcal,totalNutrients,totalNutrientsDaily) => {
+        return {
+          CHOCDF_KCAL: {
+            label: "Carbohidratos",
+            quantity: totalNutrientsKcal?.CHOCDF_KCAL?.quantity || 0,
+            unit: totalNutrientsKcal.CHOCDF_KCAL.unit,
+          },
+          FIBTG: {
+            label: "Fibra",
+            quantity: totalNutrients?.FIBTG?.quantity || 0,
+            unit: totalNutrients.FIBTG.unit,
+          },
+          SUGAR: {
+            label: "Azúcar",
+            quantity: totalNutrients?.SUGAR?.quantity || 0,
+            unit: totalNutrients.SUGAR.unit,
+          },
+          PROCNT_KCAL: {
+            label: "Proteínas",
+            quantity: totalNutrientsKcal?.PROCNT_KCAL?.quantity || 0,
+            unit: totalNutrientsKcal.PROCNT_KCAL.unit,
+          },
+          FAT_KCAL: {
+            label: "Grasas",
+            quantity: totalNutrientsKcal?.FAT_KCAL?.quantity || 0,
+            unit: totalNutrientsKcal.FAT_KCAL.unit,
+          },
+          ENERC_KCAL: {
+            label: "Energía",
+            quantity: totalNutrientsKcal?.ENERC_KCAL?.quantity || 0,
+            unit: totalNutrientsKcal.ENERC_KCAL.unit,
+            
+          },
+          FASAT: {
+            label: "Grasas Saturadas",
+            quantity: totalNutrients?.FASAT?.quantity || 0,
+            unit: totalNutrients.FASAT.unit,
+          },
+          FAPU: {
+            label: "Grasas Poliinsaturadas",
+            quantity: totalNutrients?.FAPU?.quantity || 0,
+            unit: totalNutrients.FAPU.unit,
+          },
+          FAMS: {
+            label: "Grasas Monoinsaturadas",
+            quantity: totalNutrients?.FAMS?.quantity || 0,
+            unit: totalNutrients.FAMS.unit,
+          },
+          CHOLE: {
+            label: "Colesterol",
+            quantity: totalNutrients?.CHOLE?.quantity || 0,
+            unit: totalNutrients.CHOLE.unit,
+          },
+          NA: {
+            label: "Sodio",
+            quantity: totalNutrients?.NA?.quantity || 0,
+            unit: totalNutrients.NA.unit,
+          },
+          K: {
+            label: "Potasio",
+            quantity: totalNutrients?.K?.quantity || 0,
+            unit: totalNutrients.K.unit,
+          },
+          
+          VITA_RAE: {
+            label: "Vitamina A",
+            quantity: totalNutrientsDaily?.VITA_RAE?.quantity || 0,
+            unit: totalNutrientsDaily.VITA_RAE.unit,
+          },
+        };
+      };
+    const infoTotalMacros = getInfoTotalMacro(totalNutrientsKcal,totalNutrients,totalNutrientsDaily);
+    return infoTotalMacros;
+
+    
+
 } catch (error) {
     console.error('Error al obtener los datos nutricionales', error);
 }
--- proyecto-final-master-prometeo/pages/NutriotionalInfo/NutritionalInfo.js
+++ proyecto-final-master-prometeo/pages/NutriotionalInfo/NutritionalInfo.js
@@ -16,8 +16,9 @@ export const NutritionalInfo = ()=>{
   <label for="quantity">Cantidad:</label>
   <select id="quantity" name="quantity" required>
       
-      <option value="1">1</option>
+      
       <option value="10">10</option>
+      <option value="25">25</option>
       <option value="50">50</option>
       <option value="100">100</option>
     </select>
@@ -57,25 +58,32 @@ form.addEventListener("submit", async (event)=>{
         const result = await dataNutritional(infoQuantity, infoUnit, englishKeyword);
        
 
-            const printNutritionalInfo = ()=>{
+            const printNutritionalInfo = (result)=>{
     
                 const resultsContainer = document.querySelector("#results-container");
                 
                 resultsContainer.innerHTML = `
                 <div class="divContainer">
-              <h2>Tabla Nutricional</h2>
-              <h3>${infoFood}</h3>
-              <p>Valor nutricional en ${infoQuantity} ${nameUnit} de ${infoFood}</p>
-              <h4>Energia: ${result.calories.toFixed(0)} kcal</h4>
-              <h4>Proteína: ${result.protein.toFixed(2)} g</h4>
-              <h4>Grasas: ${result.fat.toFixed(2)} g</h4>
-              <h4>Carbohidratos: ${result.carbohydrates.toFixed(2)} g</h4>
-              <h4>Fibra: ${result.fiber.toFixed(2)} g</h4>
-              <h4>Azucar: ${result.sugar.toFixed(2)} g</h4>
+              <h2>Info. Nutricional</h2>
+              <h3>Tamaño de la Porción <span>${infoQuantity} ${nameUnit}</span></h3>
+              <h4>Por ${nameUnit}</h4>
+              <h4>Energía: ${result.ENERC_KCAL?.quantity?.toFixed(0) || 'N/A'} ${result.ENERC_KCAL.unit}</h4>
+              <h4>Proteína: ${result.PROCNT_KCAL?.quantity?.toFixed(2) || 'N/A'} ${result.PROCNT_KCAL.unit}</h4>
+              <h4>Carbohidratos: ${result.CHOCDF_KCAL?.quantity?.toFixed(2) || 'N/A'} ${result.CHOCDF_KCAL.unit}</h4>
+              <h4>Fibra: ${result.FIBTG?.quantity?.toFixed(2) || 'N/A'} ${result.FIBTG.unit}</h4>
+              <h4>Azúcar: ${result.SUGAR?.quantity?.toFixed(2) || 'N/A'} ${result.SUGAR.unit}</h4>
+              <h4>Grasa: ${result.FAT_KCAL?.quantity?.toFixed(2) || 'N/A'} ${result.FAT_KCAL.unit}</h4>
+              <h4>Grasas Saturadas: ${result.FASAT?.quantity?.toFixed(2) || 'N/A'} ${result.FASAT.unit}</h4>
+              <h4>Grasas Poliinsaturadas: ${result.FAPU?.quantity?.toFixed(2) || 'N/A'} ${result.FAPU.unit}</h4>
+              <h4>Grasas Monoinsaturadas: ${result.FAMS?.quantity?.toFixed(2) || 'N/A'} ${result.FAMS.unit}</h4>
+              <h4>Colesterol: ${result.CHOLE?.quantity?.toFixed(2) || 'N/A'} ${result.CHOLE.unit}</h4>
+              <h4>Sodio: ${result.NA?.quantity?.toFixed(2) || 'N/A'} ${result.NA.unit}</h4>
+              <h4>Potasio: ${result.K?.quantity?.toFixed(2) || 'N/A'} ${result.K.unit}</h4>
+              <h4>Vitamina A: ${result.VITA_RAE?.quantity?.toFixed(2) || 'N/A'} ${result.VITA_RAE.unit}</h4>
+              
             </div>`;
 
-            totalNutrients.FIBTG.quantity
-        
+            
             }
             document.querySelector("#food").value = "";
             printNutritionalInfo(result);
