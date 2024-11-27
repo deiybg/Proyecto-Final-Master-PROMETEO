bccee1b0633278e28f23ff490860b3f39a83115d
Deibyg <40776476+deiybg@users.noreply.github.com>
2024-11-28 00:37:29 +0100

    fixed routes

--- proyecto-final-master-prometeo/main.js
+++ proyecto-final-master-prometeo/main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import Header from "./components/Header/Header";
-import { Recipes } from "./pages/Recipes/Recipes";
+import { Recipes } from "/pages/Recipes/Recipes";
 
 import Footer from "./components/Footer/Footer";
 import { router } from "./Utils/router";
